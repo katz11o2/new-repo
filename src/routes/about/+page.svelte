1d3df17213dc6f6ec721f1f76c61befecf774fc5
katz11o2 <Thiruvenkat.er@gmail.com>
2025-07-20 02:56:25 +0530

    Fix: logo paths for Vercel deployment

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -352,7 +352,7 @@
       <div class="flow-diagram">
         <div class="box-hod"><a>All Hods</a></div>
         <div class="arrow-up"></div>
-        <div class="box-hod"><a href="/head-cic">Head - CIC</a></div>
+        <div class="box-hod"><a href="/cichead">Head - CIC</a></div>
         <div class="arrow-up"></div>
         <div class="box-hod"><a href="/princi">Principal</a></div>
         <div class="arrow-up"></div>
