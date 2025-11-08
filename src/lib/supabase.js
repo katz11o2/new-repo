3a2635e2952082dad0d4a8bed6ed06ff8f8ca4ff
katz11o2 <Thiruvenkat.er@gmail.com>
2025-11-08 10:35:39 +0530

    Fix: logo paths for Vercel deployment

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 // src/lib/supabase.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://zuylqvflubicsuorflzg.supabase.co'; // replace this
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1eWxxdmZsdWJpY3N1b3JmbHpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI3NDE4MTUsImV4cCI6MjA2ODMxNzgxNX0.gWRflvVOt1lDZdrWYiHH5diBv0atmkI3vWJ5h3z3YQQ'; // replace this
+const supabaseUrl = 'https://cllmmiuzdzoajmwxiypb.supabase.co'; // replace this
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsbG1taXV6ZHpvYWptd3hpeXBiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE1NjM5NDMsImV4cCI6MjA3NzEzOTk0M30.VuuVJWxpwvV2TJYKB3rbbgqVb3gmErBpSL3llr7EtLA'; // replace this
 export const supabase = createClient(supabaseUrl, supabaseKey);
